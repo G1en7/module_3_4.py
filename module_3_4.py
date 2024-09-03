d30811f276db5fb4f1b6eb09aa3ee935905faec2
G1en7 <belkin.glebaty@mail.ru>
2024-09-03 15:20:50 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,12 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+
+    for i in other_words:
+        if root_word in i or i in root_word:
+                same_words.append(i)
+    return same_words
+
+result1 = single_root_words('rich', 'riches', 'richly', 'richie', 'ruc')
+result2 = single_root_words('disablement', 'able', 'mable', 'disable', 'bagel')
+print(result1)
+print(result2)
